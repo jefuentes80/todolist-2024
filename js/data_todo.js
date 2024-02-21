177c9b6a0f8315ae771287377cde78563bcf4fd8
jefuentes <jefuentes@scl.edu.gt>
2024-02-21 13:40:43 -0600

    botón crear completado

--- js/data_todo.js
+++ js/data_todo.js
@@ -4,21 +4,7 @@ let tareas = [
         id: 1,
         texto: "Quitar todos los tornillos de la tap",
     },
-    {
-        estado: false,
-        id: 2,
-        texto: "levantar la tapa con cuidado",
-    },
-    {
-        estado: true,
-        id: 3,
-        texto: "Desconectar la batería",
-    },
-    {
-        estado: false,
-        id: 4,
-        texto: "Identificar componentes",
-    }
+
 ]
 
 export { tareas };
